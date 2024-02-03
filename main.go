8d29783dbad7ac4da59481cc24d33e2a3c7ce0b4
Isaac Batista <isaacbatst@gmail.com>
2024-02-03 11:39:53 -0300

    connect to database

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	config := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   os.Getenv("DBADDR"),
+		DBName: "recordings",
+	}
+
+	var err error
+	db, err = sql.Open("mysql", config.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected!")
+}
