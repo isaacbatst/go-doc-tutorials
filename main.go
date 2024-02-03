cd5aed29fadb68c4f5c947a89119018c29e9c5b4
Isaac Batista <isaacbatst@gmail.com>
2024-02-03 11:40:21 -0300

    get albums by artist

--- main.go
+++ main.go
@@ -11,6 +11,13 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 func main() {
 	config := mysql.Config{
 		User:   os.Getenv("DBUSER"),
@@ -33,4 +40,33 @@ func main() {
 	}
 
 	fmt.Println("Connected!")
+
+	albums, err := albumsByArtist("John Coltrane")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Albuns found: %v\n", albums)
+}
+
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
