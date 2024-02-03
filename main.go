6b7436fa5b8ae1ba35ef54d0754b6f64c21cb00f
Isaac Batista <isaacbatst@gmail.com>
2024-02-03 11:40:40 -0300

    get album by id

--- main.go
+++ main.go
@@ -48,6 +48,13 @@ func main() {
 	}
 
 	fmt.Printf("Albuns found: %v\n", albums)
+
+	alb, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Album found: %v\n", alb)
 }
 
 func albumsByArtist(name string) ([]Album, error) {
@@ -69,4 +76,19 @@ func albumsByArtist(name string) ([]Album, error) {
 		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
 	}
 	return albums, nil
+}
+
+func albumByID(id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+
+	return alb, nil
 }
